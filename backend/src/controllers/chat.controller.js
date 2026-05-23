84bf891c9752df7fc24c806010ba571dd1a8922a
naamyuvraj <naam.yuvraj@gmail.com>
2026-05-24 02:28:58 +0530

    fix: correct User model import in chat controller

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,5 +1,5 @@
 import { Message } from "../models/message.model.js";
-import { User } from "../models/user.model.js";
+import User from "../models/user.model.js";
 import mongoose from "mongoose";
 
 export const ChatController = {
